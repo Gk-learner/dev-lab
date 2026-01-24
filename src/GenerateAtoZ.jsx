a9e8a8997e9eedbd00612d63d84144bcd744ff61
Gagan <gagansasan91@yahoo.com>
2026-01-23 20:20:39 -0700

    Duplicates removed

--- src/GenerateAtoZ.jsx
+++ src/GenerateAtoZ.jsx
@@ -1,4 +1,4 @@
-export default function generateAtoZ() {
+ const generateAtoZ =()=> {
   const arr = [];
 
   for (let i = 65; i <= 90; i++){
@@ -10,3 +10,16 @@ export default function generateAtoZ() {
 
 }
 generateAtoZ();
+
+
+const RemoveDuplicates = (arr) => {
+    const result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (!result.includes(arr[i])) {
+            result.push(arr[i]);
+        }   
+    }
+    return result;
+}
+
+export { RemoveDuplicates, generateAtoZ };
