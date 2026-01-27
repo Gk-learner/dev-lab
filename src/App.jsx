c1490737227c7945efb0b98dfed069857d3379f2
Gagan <gagansasan91@yahoo.com>
2026-01-26 22:22:40 -0700

    fibonacci

--- src/App.jsx
+++ src/App.jsx
@@ -120,7 +120,7 @@ function App() {
       </Routes>
     </Router>
     {/* <LeapYear /> */}
-    <Todo/>
+    {/* <Todo/> */}
     </>
   )
 }
--- src/GenerateAtoZ.jsx
+++ src/GenerateAtoZ.jsx
@@ -36,4 +36,16 @@ const formLargestNumber = (arr) => {
     return result[0]==="0" ? "0" : result   ;
 }
 
+function generateFibonacci(n) {
+   if (n <= 0 || !Number.isInteger(n)) return [];
+    if (n === 1) return [0]
+    const arr = [0, 1]
+    for (let i = 2; i < n; i++) {
+      
+        arr.push(arr[i-1]+arr[i-2])
+    }
+    console.log(arr)
+}
+generateFibonacci(5)
+
 export { RemoveDuplicates, generateAtoZ, flattenArray, formLargestNumber };
